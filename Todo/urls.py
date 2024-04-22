bc4892f280d218d903736b1f39b9e4768cf3f503
Aishwarya R <aishwaryaravi2002@gmail.com>
2024-04-22 12:05:20 +0530

    mytodoapp initialized

--- Todo/urls.py
+++ Todo/urls.py
@@ -3,4 +3,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('addTask/',views.addTask,name='addTask'),
+    path('mark_as_done/<int:pk>/',views.markdone,name='mark_as_done'),
+    path('mark_undo/<int:pk>/',views.markundo,name='mark_undo'),
+    path('editTask/<int:pk>/',views.editTask,name="editTask"),
+    path('deleteTask/<int:pk>/',views.deleteTask,name='deleteTask'),
+    
+    
+    
 ]
--- Todo/views.py
+++ Todo/views.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from todo.models import Task
 
 # Create your views here.
 def home(request):
-    tasks=Task.objects.all().order_by('-updated_at') # for ascending order just type order_by('updated_at')
-    
+    tasks=Task.objects.all().order_by('-updated_at') # for ascending order just type order_by('updated_at')   
     context={
         'tasks':tasks,
     }
@@ -17,3 +16,36 @@ def addTask(request):
     task=request.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+def markdone(request,pk):
+    task=get_object_or_404(Task, pk=pk)
+    task.is_completed=True
+    task.save()
+    return redirect('home')
+
+def markundo(request,pk):
+    task=get_object_or_404(Task, pk=pk)
+    task.is_completed=False
+    task.save()
+    return redirect('home')
+
+def editTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    
+    if request.method == 'POST':
+        task.task = request.POST.get('task')
+        task.save()
+        return redirect('home')  # Redirect to the home page after successful update
+    
+    context = {'task': task}
+    return render(request, 'edit.html', context)
+
+
+
+def deleteTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+    
+   
+    
