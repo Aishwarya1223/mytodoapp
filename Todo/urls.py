b470a58080b6cc22f94f3506c0d2ffed8e054430
Aishwarya R <aishwaryaravi2002@gmail.com>
2024-04-22 10:26:30 +0530

    mytodoapp initialized

---
+++ Todo/urls.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('addTask/',views.addTask,name='addTask'),
+]
--- Todo/views.py
+++ Todo/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from todo.models import Task
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    tasks=Task.objects.all().order_by('-updated_at') # for ascending order just type order_by('updated_at')
+    
+    context={
+        'tasks':tasks,
+    }
+    return render(request,'home.html',context)
+
+
+def addTask(request):
+    task=request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
